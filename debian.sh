099dc36e1bde1ef380bbec4150d623c235e12b85
adrianchaney <adrian.chaney@gmail.com>
2023-12-09 21:34:59 -0700

    add cron

--- debian.sh
+++ debian.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # ensure up to date repos
-sudo apt update --fix-missing && sudo apt update 
+sudo apt update --fix-missing && sudo apt update
 
 # system
-sudo apt install htop tree tmux curl fping -y 
+sudo apt install htop tree tmux curl fping -y
 
 # network
-sudo apt install bmon ethtool nmap iperf iperf3 netperf -y 
+sudo apt install bmon ethtool nmap iperf iperf3 netperf -y
+
+# work temp
+sudo apt install gcc make perl subversion cron awscli
